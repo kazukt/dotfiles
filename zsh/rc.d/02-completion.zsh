cf6e02bb6b345a5087fa8e2d26863fe78ecb582b
kazukt <kazuki.060525@gmail.com>
2022-05-08 17:40:52 +0900

    zsh: add zsh completion

--- zsh/rc.d/02-completion.zsh
+++ zsh/rc.d/02-completion.zsh
@@ -4,4 +4,9 @@
 # Completion settings
 #
 
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+  source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+fi
+
 autoload -Uz compinit && compinit
